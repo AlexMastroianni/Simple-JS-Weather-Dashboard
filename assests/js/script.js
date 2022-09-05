6a775ee56ee7a18bcfb78392ff4068b0e7c63e4c
AlexMastroianni <alex.m.mastroianni@gmail.com>
2022-09-05 14:11:05 +1000

    added search list

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,13 +1,16 @@
 var citySearch = [];
-var seachHistory = "";
+var userSearchHistory = "";
+var currentCity;
+var searchList = document.querySelector("#searchList");
 var userCity = document.querySelector("#cityName");
+var userSearchHistory =
+  JSON.parse(window.localStorage.getItem("weatherSearchHistory")) || [];
 
 var getWeather = function (city) {
   var apiUrl =
     "https://api.openweathermap.org/data/2.5/weather?q=" +
     city +
     "&appid=838ac9624152c68b6dec47d980282426&units=metric";
-
   fetch(apiUrl)
     .then(function (response) {
       if (response.ok) {
@@ -24,21 +27,38 @@ var getWeather = function (city) {
     });
 };
 
-// Temp == main.feels_like
-
-var showWeather = function (weatherData) {
+// Need to work on getting the UV radiation + the 5 Day forcast
+let showWeather = function (weatherData) {
   $("#cityTemp").text("Temperature: " + weatherData.main.temp + "°C");
   $("#cityHum").text("Humidity: " + weatherData.main.humidity + "%");
   $("#cityWind").text("Wind: " + weatherData.wind.speed + "m/s");
+
+  //   fetch(
+  //     "https://api.openweathermap.org/data/2.5/forecast?q=" +
+  //       weatherData.name +
+  //       "&appid=838ac9624152c68b6dec47d9802824266"
+  //   ).then(function (response) {
+  //     response.json().then(function (data) {
+  //       console.log(data);
+  //     });
+  //   });
 };
 
 let searchHandler = function (event) {
   event.preventDefault();
   let userInput = userCity.value;
-  console.log(userInput);
-  console.log("userInput");
+
   if (userInput) {
     getWeather(userInput);
+    var userSearchHistory = userCity.value;
+    citySearch.push(userSearchHistory);
+    localStorage.setItem("searchHistory", JSON.stringify(citySearch));
+    for (var i = 0; i < citySearch.length; i++) {
+      var li = document.createElement("li");
+      li.setAttribute("data-index", i);
+      li.textContent = citySearch[i];
+      searchList.appendChild(li);
+    }
   } else {
     alert("Please enter a city name");
   }
